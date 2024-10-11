168bd09f27ddfcaf0e08ac53d56111b4e883caa8
22simeon14 <simeonslavov715@gmail.com>
2024-10-11 15:45:24 +0300

    Update Week2_Simeon.cpp

--- Seminar02/Week2_Simeon.cpp
+++ Seminar02/Week2_Simeon.cpp
@@ -24,11 +24,46 @@ int main()
     }
     else cout << "The char is not in the latin alphabet!\n";*/
     
-    cout << "Exercise 3: \n";
+    /*cout << "Exercise 3: \n";
     int a, b;
     cin >> a;
     cin >> b;
     string biggerNumber = (a > b) ? "a" : "b";
-    cout << "The bigger number is : " << biggerNumber;
+    cout << "The bigger number is : " << biggerNumber;*/
+    
+    /*cout << "Exercise 4: \n";
+    cout << "Enter a grade in percentage: ";
+    int grade;
+    cin >> grade;
+    if (grade >= 0 && grade <= 100)
+    {
+        if (grade >= 90) cout << "6+";
+        else if (grade >= 80) cout << "6";
+        else if (grade >= 70) cout << "5";
+        else if (grade >= 60) cout << "4";
+        else if (grade >= 40) cout << "3";
+        else cout << "2";
+    }
+    else cout << "Invalid grade!\n";*/
+    
+    /*cout << "Exercise 5: \n";
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    if (a + b > c && a + c > b && b + c > a) cout << "Valid triangle! \n";
+    else cout << "Invalid triangle! \n";*/
+    
+    /*cout << "Exercise 6: \n";
+    cout << "Enter traffic light state: \n";
+    int trafficLightState;
+    cin >> trafficLightState;
+    switch (trafficLightState) {
+    case 1: cout << "You can pass! "; break;
+    case 0: cout << "Warning!";
+        case -1: cout << "You cannot pass!"; break;
+    case -1: cout << "You cannot pass"; break;
+    }*/
+    
 }
 
