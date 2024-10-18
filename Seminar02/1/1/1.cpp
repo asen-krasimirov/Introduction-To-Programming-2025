0a4553e2ec4a9c54264b8487da91636f7533ac05
Lionkata <leonor.yankova1@gmail.com>
2024-10-18 14:29:29 +0300

    practicum 1

---
+++ Seminar02/1/1/1.cpp
@@ -0,0 +1,168 @@
+// 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    // std::cout << "Oh what\n a happy day! \n Oh yes, \n what a happy day! \n"; //purva
+
+        /*double a = 5.4;
+        double b = 7.9;
+        double c = 2 * (a + b);
+        double p = a * b;
+        std::cout << c << '\n';
+        std::cout << p;*/ //vtora
+
+    //treta
+        /*int a ;
+         std :: cin >>a
+        double b = 0.56;
+        double c = 0.51;
+        double d = a * b;
+        double f = a * c;
+        std::cout << d << '\n';
+        std::cout << f;*/
+
+    //chetvurta
+        /*std::cout << "Please enter the length of the first side:" << '\n';
+         double a;
+         std::cin >> a;
+         double b;
+         std::cin >> b;
+         double P = 2*(a + b);
+         double s = a * b;
+         std::cout <<  P << '\n';
+         std::cout << s;*/
+
+    //peta
+         /*int a;
+         std::cin >> a;
+         int b;
+         std::cin >> b;
+         bool shouldSwap = (a < b) && (a = b);
+         std::cout << shouldSwap;*/
+
+    //shesta
+         /*int a;
+          std::cout << "Dividend:";
+          std::cin >> a;
+          int b;
+          std::cout << "Divisor:";
+          std::cin >> b;
+          std::cout << "The quotient of the division is :" << a / b << '\n';
+          std::cout << "The remainder of the division is:" << a % b << '\n';*/
+
+    //sedma
+          /*int a;
+          int b;
+          int c;
+          std::cout << "Apples:";
+          std::cin >> a;
+          std::cout << "Pears:";
+          std::cin >> b;
+          std::cout << "Bananas:";
+          std::cin >> c;
+          std::cout << "Pesho, don't forget to buy " << a << " apples";*/
+
+    //osma
+
+   /*int r;
+    std::cin >> r;
+    double p = 3.14159265;
+    double C = 2 * r * p;
+    std::cout << C << '\n';
+    double S = r * r * p;
+    std::cout << S;*/
+
+    //deveta
+
+    /*double a;
+    double b;
+    double c;
+    std::cout << "a = ";
+    std::cin >> a;
+    std::cout << "b = ";
+    std::cin >> b;
+    std::cout << "c = ";
+    std::cin >> c;
+    double d = b * b - 4 * a * c;
+    std::cout << "x1 = ";
+    std::cout << (-b + sqrt(d)) / (a * 2) << '\n';
+    std::cout << "x2 = ";
+    std::cout << (-b - sqrt(d)) / (a * 2);*/
+
+    //deset
+    //s dopulnitelna promenliva
+   /* int a;
+    int b;
+    int q;
+    std::cin >> a;
+    std:: cin >> b;
+    std::cin >> q;
+    std::cout << a << " " << b << " " << q << '\n';
+    q = a;
+    a = b;
+    b = q;
+    std::cout << a << " " << b << " " << q;*/
+
+//bez dopulnitelna promenliva
+//int a;
+//int b;
+//std::cin >> a;
+//std::cin >> b;
+//std::cout << a << " " << b << '\n';
+//std::cout << (a + b) - a << " " << (a + b) - b;
+
+    //edinadeset ??
+
+    ///*int a;
+    //int b;
+    //std::cin >> a;
+    //std::cin >> b;
+    //int max = (a > b)? a :b;
+    //std::cout << "max = ";
+    //std::cout << max << '\n';
+    //int min = (a < b) ? a : b;
+    //std::cout << "min = ";
+    //std::cout << min;*/ 
+
+    //dvanadeseta
+
+/*int a;
+std::cout << "Seconds: ";
+std::cin >> a;
+int d = a /60/60/24;
+
+int h = (a - (d*60*60*24))/60/60 ;
+int m = ((a - (d * 60 * 60 * 24)) - h*60*60)/60;
+int s = ((a - (d * 60 * 60 * 24)) - h * 60 * 60) - m*60;
+std::cout << "Days: ";
+std::cout << d;
+std::cout << " Hours: ";
+std::cout << h;
+std::cout << " Minutes: ";
+std::cout << m;
+std::cout << " Seconds: ";
+std::cout << s;
+*/
+
+
+// trinadeseta 
+double a;
+double b;
+std::cin >> a;
+std::cin >> b;
+std::cout << std::fixed;
+std::cout << std::setprecision(c);
+std::cout << a + b;
+
+//std::cout  << a + b << printf("%.2f");
+
+//chetirinadeset
+int n;
+int sum = 0;
+
+
+}
+
