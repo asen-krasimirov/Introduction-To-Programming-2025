84c7aa5262ea41282390783374682a300c2b9fce
DobromirPeev <102480232+DobromirNPeev@users.noreply.github.com>
2024-10-18 08:07:24 +0300

    Update Week2_Simeon.cpp

--- Seminar02/solutions/Week2_Simeon.cpp
+++ Seminar02/solutions/Week2_Simeon.cpp
@@ -1,1 +1,69 @@
 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*cout << "Exercise 1: \n";
+    int year;
+    cin >> year;
+    if (year % 4 == 0) cout << "The year is visokosna!\n";
+    else cout << "Godinata ne e visokosna!\n";*/
+    
+    /*cout << "Exersice 2: \n";
+    char c;
+    cin >> c;
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+    {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+        {
+            cout << "The letter is vowel!\n";
+        }
+        else cout << "The letter is not vowel!\n";
+    }
+    else cout << "The char is not in the latin alphabet!\n";*/
+    
+    /*cout << "Exercise 3: \n";
+    int a, b;
+    cin >> a;
+    cin >> b;
+    string biggerNumber = (a > b) ? "a" : "b";
+    cout << "The bigger number is : " << biggerNumber;*/
+    
+    /*cout << "Exercise 4: \n";
+    cout << "Enter a grade in percentage: ";
+    int grade;
+    cin >> grade;
+    if (grade >= 0 && grade <= 100)
+    {
+        if (grade >= 90) cout << "6+";
+        else if (grade >= 80) cout << "6";
+        else if (grade >= 70) cout << "5";
+        else if (grade >= 60) cout << "4";
+        else if (grade >= 40) cout << "3";
+        else cout << "2";
+    }
+    else cout << "Invalid grade!\n";*/
+    
+    /*cout << "Exercise 5: \n";
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    if (a + b > c && a + c > b && b + c > a) cout << "Valid triangle! \n";
+    else cout << "Invalid triangle! \n";*/
+    
+    /*cout << "Exercise 6: \n";
+    cout << "Enter traffic light state: \n";
+    int trafficLightState;
+    cin >> trafficLightState;
+    switch (trafficLightState) {
+    case 1: cout << "You can pass! "; break;
+    case 0: cout << "Warning!";
+        case -1: cout << "You cannot pass!"; break;
+    case -1: cout << "You cannot pass"; break;
+    }*/
+    
+}
+
