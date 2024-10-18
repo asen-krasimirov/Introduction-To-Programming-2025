1caa5dc62cf937c3a155a7a25efe91dfaefd709b
VICTORY637 <victoriabambalova@abv.bg>
2024-10-18 14:49:00 +0300

    prac2viki

---
+++ Seminar02/solutions/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,41 @@
+
+#include <iostream>
+
+int main() {
+
+	/*
+		int year;
+		std::cin >> year;
+		if (year % 400 == 0) {
+			std::cout << "Leap year";
+		}
+		else if (year % 100 == 0) {
+			std::cout << "Not leap year.";
+		}
+		else if (year % 4 == 0) {
+			std::cout << "Leap year";
+
+		}
+		else {
+			std::cout << "Not leap year";
+		}
+	}
+	*/
+
+
+	/*char letter;
+	std::cin >> letter;
+	if (letter >= 'a') {
+		std::cout << "Latter of the Latin alphabet";
+	}
+	//else if (letter >= 97 && <= 122)*/
+
+	/*int a;
+	std::cin >> a;
+	int b;
+	std::cin >> b;
+	int*/
+}
+	
+
+
