d3cd31f0d382d4a8cd5395701edccdcc35eb2c4a
Yord0 <yord013bg@gmail.com>
2024-10-20 21:38:00 +0300

    Pract 01 | Yordanka Ilieva

---
+++ Seminar01/Solutions/ConsoleApplication1.cpp
@@ -0,0 +1,150 @@
+﻿// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Задача 1. 
+	//cout << "Oh what" << '\n';
+	//cout << "a happy day!" << '\n';
+	//cout << "Oh yes," << '\n';
+	//cout << "what a happy day!" << '\n';
+
+	//Задача 2.
+	//double a = 5.4, b = 7.9;
+	//int P = 2*(a + b);
+	//int S = a * b;
+	//cout << "Perimeter: " << P << '\n' << "Area: " << S;
+
+	//Задача 3.
+	//double a;
+	//cout << "Levs: ";
+	//cin >> a;
+	//double Dollar = a * 0.56;
+	//double Euro = a * 0.51;
+	//cout << "Dollars: " << Dollar << '\n';
+	//cout << "Euro: " << Euro;
+
+	//Задача 4. 
+	//cout << "Please enter the length of the first side: "<< '\n';
+	//double a;
+	//cin >> a;
+	//cout << "Please enter the length of the second side: ";
+	//double b;
+	//cin >> b;
+	//double P = 2 * (a + b);
+	//double S = a * b;
+	//cout << "Perimeter: " << P << "  Area: " << S;
+
+	// Задача 5. 
+	//int a = 1, b = 1;
+  // cin >> a >> b;
+   //bool Small = a < b;
+   // cout << Small * 1 + !Small * 0;
+
+	//Задача  6.
+   // int a = 1, b = 1;
+   // cout << "Dividend: ";
+   // cin >> a;
+  //  cout << "Divisor: ";
+  //  cin >> b;
+  //  int Quotient = a / b;
+   // int Remainder = a % b;
+  //  cout << "The quotient of the division is : ";cout << Quotient;
+  //  cout << '\n' << "The remainder of the division is : ";cout << Remainder;
+
+	//Задача 7.
+	//int a, b, c;
+	//cout << "Apples: ";
+	//cin >> a;
+	//cout << "Pears: ";
+	//cin >> b;
+	//cout << "Bananas: ";
+	//cin >> c;
+	//cout << "Pesho, don’t forget to buy " << a << " apples, " << b << " pears and " << c << " bananas!";
+
+	//Задача 8.
+	//double r;
+	//cin >> r;
+	//const PI = 3.14159265;
+	//double C = 2 * PI *r;
+	//double S = PI * r * r;
+	//cout << C << '\n';
+	//cout << S;
+
+														//Задача 9. Решена с малко помощ
+	//double a, b, c, x1, x2;
+	//cout << "a = ";
+	//cin >> a;
+	//cout << "b = ";
+	//cin >> b;
+	//cout << "c = ";
+	//cin >> c;
+	//double Discriminant = b * b - 4 * a * c;
+
+
+	//if (Discriminant > 0) {
+	//	x1 = (-b - sqrt(Discriminant)) / (2 * a);
+	//	x2 = (-b + sqrt(Discriminant)) / (2 * a);
+	//
+	//	cout << "x1 = " << x1 << ", x2 = " << x2;
+	//}
+	//else if (Discriminant == 0) {
+	//	x1 = -b / 2 * a;
+	//	cout << "x1,2 = " << x1;
+	//}
+//
+	//if (Discriminant < 0) {
+	//	cout << "no real solutions";
+	//}
+
+
+
+														//Задача 10.1 с допълнителна променлива (леко затруднение)
+	//int a, b, c;
+	//cin >> a >> b;
+	//c = b;
+	//b = a;
+	//a = c;
+	//cout << a << endl << b;
+	                                                   //Задача 10.2 без допълнителна променлива  - помощ
+
+
+
+
+	                                                  //Задача 11 (леко затруднение)
+//int a, b;
+//cin >> a >> b;
+//bool min = a < b;
+//cout << "Min " << min*a + !min*b << endl;
+//cout << "Max " << !min * a + min * b;
+
+//Задача 12
+ // int a;
+ // cout << "Seconds: ";
+ // cin >> a;
+ // int Seconds = a % 60;
+ // int Minutes = (a / 60) % 60;
+ // int Hours = (a / 3600) % 24;
+ // int Days = (a / 3600) / 24;
+ // cout << Days << " days, " << Hours << " hours, " << Minutes << " minutes and " << Seconds << " seconds";
+
+ //Задача 13
+ //int x1, y1, x2, y2;
+ //cout << "Number 1: ";
+ //cin >> x1 >> y1;
+ //cout << "Number 2: ";
+ //cin >> x2 >> y2;
+ //double d = sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+ //cout << "Distance: " << d;
+
+	 //Задача 14
+	 // int n;
+	 // cin >> n;
+	 // int S = n * (n + 1) / 2;
+	 // cout << S;
+
+}
+
